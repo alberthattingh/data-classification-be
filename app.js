93058890ba2535ba3d6bf640bf77f3eb6f5c761f
Albert Hattingh <albert.h0504@gmail.com>
2020-11-15 21:45:32 +0200

    File uploads now supported
    TODO: Work on response sent after file is uploaded.

--- app.js
+++ app.js
@@ -25,6 +25,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/classify', classifyRouter);
+// app.use('/uploads', express.static('uploads')); // uncomment if you want files to be publicly accessible
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/classify.js
+++ routes/classify.js
@@ -1,6 +1,34 @@
 var express = require('express');
 var router = express.Router();
 const jwt = require('jsonwebtoken');
+const multer = require('multer');
+
+// Set configurations for storing uploaded files with multer
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'uploads/');
+    },
+    filename: (req, file, cb) => {
+        cb(null, Date.now() + file.originalname)
+    }
+});
+
+// Set filter to only accept certain file types for upload
+const fileFilter = (req, file, cb) => {
+    if (file.mimeType === "text/plain" ||
+        file.mimeType === "text/csv" ||
+        file.mimeType === "application/json" ||
+        file.mimeType === "application/vnd.ms-excel" ||
+        file.mimeType === "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet") {
+        // accept these files -- save and classify
+        cb(null, true); // continue with the file without throwing an error
+    }
+    else {
+        cb(null, false); // dont continue with the file, but dont throw error either
+    }
+};
+
+const upload = multer({storage: storage, fileFilter: fileFilter});
 
 /* Middleware -- runs before request is handled -- to authenticate access token */
 function authenticateToken(req, res, next) {
@@ -10,7 +38,7 @@ function authenticateToken(req, res, next) {
         return res.sendStatus(401);
 
     jwt.verify(token, process.env.ACCESS_TOKEN_SECRET, (err, user) => {
-        console.log(err);
+        // console.log(err);
         if (err)
             return res.sendStatus(403);
         req.user = user;
@@ -18,8 +46,12 @@ function authenticateToken(req, res, next) {
     });
 }
 
-/* GET home page. */
-router.get('/', authenticateToken, (req, res) => {
+
+router.post('/', authenticateToken, upload.single('dataFile'), (req, res) => {
+    const username = req.user.username;
+    const file = req.file;
+    console.log(file);
+
     res.json({username: req.user.username, data: 'Some data'});
 });
 
