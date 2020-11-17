8414d8a0b70ded17ed06acd88e8b1c64cb1006e8
Albert Hattingh <albert.h0504@gmail.com>
2020-11-17 14:02:06 +0200

    Successfully parse xlsx, csv, & txt files
    Still have to classify data and support xls files. But at the moment the above files can be parsed and the data they contain is sent back as a response.

--- routes/classify.js
+++ routes/classify.js
@@ -1,7 +1,10 @@
 var express = require('express');
 var router = express.Router();
+const fs = require('fs');
 const jwt = require('jsonwebtoken');
 const multer = require('multer');
+const readExcel = require('read-excel-file/node');
+const Field = require('../models/field');
 
 // Set configurations for storing uploaded files with multer
 const storage = multer.diskStorage({
@@ -15,11 +18,11 @@ const storage = multer.diskStorage({
 
 // Set filter to only accept certain file types for upload
 const fileFilter = (req, file, cb) => {
-    if (file.mimeType === "text/plain" ||
-        file.mimeType === "text/csv" ||
-        file.mimeType === "application/json" ||
-        file.mimeType === "application/vnd.ms-excel" ||
-        file.mimeType === "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet") {
+    if (file.mimetype === "text/plain" ||
+        file.mimetype === "text/csv" ||
+        // file.mimetype === "application/json" ||
+        file.mimetype === "application/vnd.ms-excel" ||
+        file.mimetype === "application/vnd.openxmlformats-officedocument.spreadsheetml.sheet") {
         // accept these files -- save and classify
         cb(null, true); // continue with the file without throwing an error
     }
@@ -46,13 +49,83 @@ function authenticateToken(req, res, next) {
     });
 }
 
+// Function to read and parse an excel file into an array of fields
+async function readExcelFileIntoRows(file) {
+    return readExcel(file.path).then((rows) => {
+        const newRows = [];
+        for (let row of rows) {
+            const columns = [];
+            for (let i = 0; i < row.length; i++) {
+                const field = new Field(i, row[i]);
+                columns.push(field);
+            }
+            newRows.push(columns);
+        }
+        // console.table(newRows);
+        return newRows;
+    });
+}
 
+// Function to read and parse a text file into an array of fields
+async function readTextFileIntoRowsOfFields(file, sep) {
+    const fileAsString = fs.readFileSync(file.path).toString();
+    const lines = fileAsString.split('\n');
+
+    const rows = [];
+    for (let line of lines) {
+        const divs = line.split(sep);
+        const columns = [];
+        for (let i = 0; i < divs.length; i++) {
+            const field = new Field(i, divs[i]);
+            columns.push(field);
+        }
+        rows.push(columns);
+    }
+    // console.table(rows);
+    return rows;
+}
+
+// Function that classifies the data in the file
+async function classify(file) {
+    if (file.originalname.endsWith('.xlsx') || file.originalname.endsWith(".xls")) {
+        const rowsOfFields = await readExcelFileIntoRows(file);
+        return rowsOfFields;
+    }
+    else if (file.originalname.endsWith('.txt')) {
+        const rowsOfFields = await readTextFileIntoRowsOfFields(file, '|');
+        return rowsOfFields;
+    }
+    else if (file.originalname.endsWith('.csv')) {
+        const rowsOfFields = await readTextFileIntoRowsOfFields(file, ',');
+        return rowsOfFields;
+    }
+    else if (file.originalname.endsWith('.json')) {
+        // Not supported at the moment -- add support at later stage
+        return [];
+    }
+    else {
+        // This is not supposed to ever be run
+        // Files are filtered to allow only the above types
+        // throw new Error('Unsupported file type');
+        return [];
+    }
+}
+
+
+/* The actual endpoint -- handles POST request to classify data in a file */
 router.post('/', authenticateToken, upload.single('dataFile'), (req, res) => {
     const username = req.user.username;
     const file = req.file;
-    console.log(file);
+    // console.log(file);
 
-    res.json({username: req.user.username, data: 'Some data'});
+    classify(file).then((classifiedData) => {
+        if (classifiedData.length > 0) {
+            res.json({username: req.user.username, data: classifiedData});
+        }
+        else {
+            res.json({username: req.user.username, data: classifiedData});
+        }
+    });
 });
 
 module.exports = router;
